c04a153e6767c0631bd367f4446a74e938044cc2
1GJones <jonesgamal73@gmail.com>
2025-02-27 21:58:05 -0500

    Added TextNode class and tested in main.py

--- public/src/main.py
+++ public/src/main.py
@@ -1,1 +1,10 @@
 print("Hello, World")
+from textnode import TextNode, TextType
+
+def main():
+    """Main function to test the TextNode class."""
+    node = TextNode("This is a text node", TextType.BOLD, "httos://www.boot.dev")
+    print(node)
+    
+    if __name__ == "__main__":
+        main()
