ea7bd8afed5fb813b2229f34b33c294bbe3bec97
1GJones <jonesgamal73@gmail.com>
2025-03-14 21:07:51 -0400

    Fixed TextNode

--- public/src/textnode.py
+++ public/src/textnode.py
@@ -1,8 +1,10 @@
 from enum import Enum
+
+from htmlnode import LeafNode
 class TextType(Enum):
     
     """Enum representing different types of text nodes."""
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -38,10 +40,33 @@ class TextNode:
         """Returns a string representation of the TextNode."""
         return f"TextNode(text=({self.text!r},{self.text_type},{self.url!r})"
     
+    
+    
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type== TextType.TEXT:
+        return LeafNode(None,text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text,)
+    elif text_node.text_type == TextType.LINK:   
+        return LeafNode("a", text_node.text, {"href":text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        # Assuming text_node has 'src' and 'alt' attributes for images
+        return LeafNode("img", "", {"src": text_node.src, "alt": text_node.alt})
+    else:
+        raise Exception(f"Invalid text type: {text_node.text_type}")
+   
+
+
+
 if __name__ == "__main__":
-    node1 = TextNode("Hello", TextType.NORMAL)
-    node2 = TextNode("Hello", TextType.NORMAL)
-    node3 = TextNode("Different", TextType.NORMAL)
+    node1 = TextNode("Hello", TextType.TEXT)
+    node2 = TextNode("Hello", TextType.TEXT)
+    node3 = TextNode("Different", TextType.TEXT)
     node4 = TextNode("Hello", TextType.LINK, "https://example.com")
 
     # Equality tests
